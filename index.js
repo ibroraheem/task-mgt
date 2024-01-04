75f27036a9feed11323fea70e0418968881e4df6
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2024-01-04 07:39:04 +0100

    Task Routes

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const connectDb = require('./config/db');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'x-access-token', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Headers', 'Access-Control-Request-Headers', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Credentials'],
+    }
+));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
